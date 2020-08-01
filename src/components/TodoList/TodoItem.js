98da48a0e7d9ab573d0db8a2a5eb8b0e09c832e6
卢本伟牛批 <panlinfeng@hive-intel.com>
2020-08-01 09:58:50 +0800

    生命周期学习

--- src/components/TodoList/TodoItem.js
+++ src/components/TodoList/TodoItem.js
@@ -1,14 +1,47 @@
 import React, { Component } from "react"
 const noop = () => {}
 export default class TodoItem extends Component {
+  constructor() {
+    super()
+    this.state = {
+      coxText: "",
+    }
+  }
+
   handleChange = () => {
     // this.props.onCompletedChange && this.props.onCompletedChange(this.props.id)
     const { onCompletedChange = noop, id } = this.props //默认值搞成空函数，保证代码运行不报错
     onCompletedChange && onCompletedChange(this.props.id)
   }
 
+  // UNSAFE_componentWillMount() {
+  //   console.log("will mount")
+  // }
+
+  // static getDerivedStateFromProps() {//与上面的只能执行其中一个，排他的
+  //   return {}
+  // }
+
+  //优化
+  shouldComponentUpdate(nextProps, nextState) {
+    // console.log(nextProps)
+    // console.log(this.props)
+
+    return nextProps.isCompleted !== this.props.isCompleted
+  }
+
+  //静态方法是没有this的，这个是在对象创建之前就存在了
+  static getDerivedStateFromProps(props, state) {
+    //根据props属性来计算
+    console.log(props, state)
+    return {
+      compltedText: props.isCompleted ? "完成" : "未完成",
+    }
+  }
+
   render() {
     const { isCompleted, title } = this.props
+    console.log("todo render", this.state.compltedText)
     return (
       <li>
         <input
@@ -24,3 +57,31 @@ export default class TodoItem extends Component {
     )
   }
 }
+
+// import React, { PureComponent } from "react"
+// const noop = () => {}
+// export default class TodoItem extends PureComponent {
+//   handleChange = () => {
+//     // this.props.onCompletedChange && this.props.onCompletedChange(this.props.id)
+//     const { onCompletedChange = noop, id } = this.props //默认值搞成空函数，保证代码运行不报错
+//     onCompletedChange && onCompletedChange(this.props.id)
+//   }
+
+//   render() {
+//     const { isCompleted, title } = this.props
+//     console.log("todo render")
+//     return (
+//       <li>
+//         <input
+//           type="checkbox"
+//           checked={isCompleted}
+//           onChange={this.handleChange}
+//         />
+//         <span>
+//           {" "}
+//           {title} {isCompleted ? "已完成" : "未完成"}
+//         </span>
+//       </li>
+//     )
+//   }
+// }
